427bda98231bd7e26ea369fe610a35b7397d980c
Mishal <mishal0404@gmail.com>
2024-03-03 00:05:19 +0530

    first commit

---
+++ server.py
@@ -0,0 +1,19 @@
+# main.py
+from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+import time
+
+app = FastAPI()
+
+
+
+
+@app.get("/finance/{ticker}/{exchange}")
+async def read_user_item(ticker: str, exchange: str):
+    url = f'https://www.google.com/finance/quote/{ticker}:{exchange}'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    classl = "YMlKec fxKbKc"
+    price = float(soup.find(class_=classl).text.strip()[1:].replace(",",""))
+    return {"nav":price}
