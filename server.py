a77b9dc0300b8b1a7b38b720b34d52f306efe905
Mishal <mishal0404@gmail.com>
2024-03-03 00:13:32 +0530

    added cors

--- server.py
+++ server.py
@@ -5,10 +5,28 @@ from bs4 import BeautifulSoup
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+# Set up CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "*"
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 
 
-@app.get("/finance/{ticker}/{exchange}")
+
+@app.get("/{ticker}/{exchange}")
 async def read_user_item(ticker: str, exchange: str):
     url = f'https://www.google.com/finance/quote/{ticker}:{exchange}'
     response = requests.get(url)
